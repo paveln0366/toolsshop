35e97e1f96115b260aa5941db8237d7d4d19fa8c
Pavel <paveln0366@gmail.com>
2021-02-20 12:46:33 +0300

    main activity created

--- app/src/main/java/com/pavelpotapov/toolsshop/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/toolsshop/MainActivity.java
@@ -1,14 +1,32 @@
 package com.pavelpotapov.toolsshop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Позиция: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
